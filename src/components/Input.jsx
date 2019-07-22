61ba5c60cba44c081fe98149b09462125d7e9241
Paulo Moisés <paulomoises_silva@hotmail.com>
2019-07-22 00:23:58 -0300

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ export default props => {
         <div>
             <label className="inp-tex" htmlFor="valor0">Quantos anos você tem?</label>
             <input className="inp-inp-tex" type="number" min="0" step=".1" id="valor0" /><br /><br />
-            <label className="inp-tex" htmlFor="valor1">Quanto você quer recebr por mês ao se aposentar?</label>
+            <label className="inp-tex" htmlFor="valor1">Quanto você quer receber por mês ao se aposentar?</label>
             <input className="inp-inp-tex" type="number" min="0" step="1" id="valor1" /><br /><br />
             <label className="inp-tex" htmlFor="valor2">Em quanto tempo você pretende apostentar?</label>
             <br/><small>Coloque o tempo em meses.</small>
